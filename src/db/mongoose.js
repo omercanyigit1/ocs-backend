a8bf898c54ca68cbd08488f9a976ab199b720894
Omer Can Yigit <62876847+omercanyigit1@users.noreply.github.com>
2022-11-19 00:30:03 +0300

    update

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb+srv://root:Omercan123@cluster0.nwaku.mongodb.net/OCS?retryWrites=true&w=majority`, {
+mongoose.connect(`dburl`, {
     useNewUrlParser: true,
     useCreateIndex: true
 });
